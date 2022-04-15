bea160de424ccaf2f06c3c010ce00a0cdb18bd5c
Mickey <hedgefund996@gmail.com>
2022-04-15 22:39:42 +0800

    comments: English

--- layer1/contract/Governance.sol
+++ layer1/contract/Governance.sol
@@ -1,16 +1,16 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// 实现了一些layer1的方法，以及我们Governance update的方法
+// 实现了一些layer1的方法，以及我们Governance update的方法. Implemented some layer1 methods, as well as our Governance update method
 import "@matterlabs/zksync-contracts/contracts/interfaces/IZkSync.sol";
-// zk operation的核心代码，未来值得研究
+// zk operation的核心代码，未来值得研究. The core code of zk operation is worth studying in the future
 import "@matterlabs/zksync-contracts/contracts/libraries/Operations.sol";
 
 contract Governance {
-    // 定义address
+    // define address
     address public governor;
 
-    // 参数：message sender
+    // parameter：message sender
     constructor() {
         governor = msg.sender;
     }
@@ -20,15 +20,15 @@ contract Governance {
         address contractAddr, 
         bytes memory data,
         uint64 ergsLimit
-        // public所有人都可以访问，external只能在外部访问（不能在内部访问 ）
-        // 添加payable，该函数被调用时允许接收ether：https://solidity-by-example.org/payable/
+        // public所有人都可以访问，external只能在外部访问（不能在内部访问 ）. Public can be accessed by everyone, external can only be accessed externally (can not be accessed internally)
+        // 添加payable，该函数被调用时允许接收ether：https://solidity-by-example.org/payable/. Add payable, which allows to receive ether when the function is called
     ) external payable {
         require(msg.sender == governor, "Only governor is allowed");
 
-        // 实例化了一个IZkSync
+        // 实例化了一个IZkSync. instantiated an i zk sync
         IZkSync zksync = IZkSync(zkSyncAddress);
         // Note that we pass the value as the fee for executing the transaction
-        // 传入msg.value、合约地址、data、ergsLimit、QueueType.Deque、OpTree.Full
+        // 传入msg.value、合约地址、data、ergsLimit、QueueType.Deque、OpTree.Full. Input msg.value, contract address, data, ergs limit, queue type.deque, op tree.full
         zksync.requestExecute{value: msg.value}(contractAddr, data, ergsLimit, Operations.QueueType.Deque, Operations.OpTree.Full);
     }
 }
--- layer1/hardhat.config.ts
+++ layer1/hardhat.config.ts
@@ -2,20 +2,20 @@ import * as dotenv from "dotenv";
 
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
-// * 华夫饼，用于测试智能合约的插件：https://github.com/TrueFiEng/Waffle
+// * 华夫饼，用于测试智能合约的插件：https://github.com/TrueFiEng/Waffle. Waffle, a plugin for testing smart contracts
 import "@nomiclabs/hardhat-waffle";
-// * typechain用typescript实现，可为智能合约提供完整的类型接口：https://github.com/dethcrypto/TypeChain
+// * typechain用typescript实现，可为智能合约提供完整的类型接口：https://github.com/dethcrypto/TypeChain. Typechain is implemented in typescript, which can provide a complete type interface for smart contracts
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
-// * solidity-coverage 是检测代码（貌似是test code）覆盖率的测试工具：https://github.com/sc-forks/solidity-coverage
+// * solidity-coverage 是检测代码（貌似是test code）覆盖率的测试工具：https://github.com/sc-forks/solidity-coverage. solidity-coverage is a testing tool for detecting code (seems to be test code) coverage
 import "solidity-coverage";
 
 const goerli = require('./goerli.json');
 
 dotenv.config();
 
-// * 所有hardhat可以完成的行为都可以被定义成一个task：https://hardhat.org/guides/create-task.html
-// * 每个task都是一个异步函数
+// * 所有hardhat可以完成的行为都可以被定义成一个task：https://hardhat.org/guides/create-task.html. All actions that can be done by hardhat can be defined as a task
+// * 每个task都是一个异步函数. Each task is an asynchronous function
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -24,16 +24,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// * hardhat config灵活性很强，可以自己定义：https://hardhat.org/config/
+// * hardhat config灵活性很强，可以自己定义：https://hardhat.org/config/. Hardhat config is very flexible and can be defined by yourself
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.0",
-    // * hardhat关于编译solidity的相关设置，详细设置见：https://docs.soliditylang.org/en/v0.7.4/using-the-compiler.html#input-description
+    // * hardhat关于编译solidity的相关设置，详细设置见：https://docs.soliditylang.org/en/v0.7.4/using-the-compiler.html#input-description. Hardhat has related settings for compiling solidity. For details, see
     settings: {
-      // * 合约gas优化，假设将被调用的次数（run），详细逻辑见：https://docs.soliditylang.org/en/v0.7.4/using-the-compiler.html#input-description
+      // * 合约gas优化，假设将被调用的次数（run），详细逻辑见：https://docs.soliditylang.org/en/v0.7.4/using-the-compiler.html#input-description. Contract gas optimization, assuming the number of times it will be called (run), see the detailed logic
       optimizer: {
         enabled: true,
-        // * 如果希望部署成本低、未来调用成本高，就把run设置为1；反之无所谓部署成本、希望调用成本低，可以把run设置为一个较高值
+        // * 如果希望部署成本低、未来调用成本高，就把run设置为1；反之无所谓部署成本、希望调用成本低，可以把run设置为一个较高值. If you want low deployment cost and high calling cost in the future, set run to 1; otherwise, you can set run to a higher value if you want low deployment cost and low calling cost
         runs: 200
       },
       outputSelection: {
@@ -45,22 +45,22 @@ const config: HardhatUserConfig = {
   },
   networks: {
     goerli: {
-      // * 导入goerli的rpc url和部署合约地址的private key
+      // * 导入goerli的rpc url和部署合约地址的private key. Import the rpc url of goerli and the private key of the deployed contract address
       url: goerli.nodeUrl,
       accounts: [goerli.deployerPrivateKey]
     },
     localhost: {
-      // * 运行 npx hardhat node 启动hardhat network时连接的测试网
+      // * 运行 npx hardhat node 启动hardhat network时连接的测试网. The test network connected when running npx hardhat node to start the hardhat network
       url: 'http://127.0.0.1:8545',
       accounts: ['0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110']
     }
   },
-  // * 一个报告gas使用量的插件：https://learnblockchain.cn/docs/hardhat/plugins/hardhat-gas-reporter.html
+  // * 一个报告gas使用量的插件：https://learnblockchain.cn/docs/hardhat/plugins/hardhat-gas-reporter.html. A plugin to report gas usage
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
   },
-  // * 用Etherscan的验证服务对合约进行检查，需要api：https://learnblockchain.cn/docs/hardhat/plugins/nomiclabs-hardhat-etherscan.html
+  // * 用Etherscan的验证服务对合约进行检查，需要api：https://learnblockchain.cn/docs/hardhat/plugins/nomiclabs-hardhat-etherscan.html. Use etherscan's verification service to check the contract, requires api
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
